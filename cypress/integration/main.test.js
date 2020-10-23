2e0827302bc40dbc4daa4bf6042362df4e5ab872
weizhongtan <me@wztan.com>
2020-10-24 00:05:57 +0100

    more tests

--- cypress/integration/main.test.js
+++ cypress/integration/main.test.js
@@ -5,6 +5,10 @@ describe('Main', () => {
     cy.visit('/');
   });
 
+  it('sets a default name', () => {
+    cy.get('input[name=name]').should('have.value', 'No name set');
+  });
+
   it('returns an error if no room exists', () => {
     cy.get('input[name=gameId]').type('ABCD').should('have.value', 'ABCD');
 
@@ -23,6 +27,24 @@ describe('Main', () => {
     cy.get('input[name=gameId]')
       .invoke('val')
       .should('match', /[a-z0-9]{4}/);
+
+    cy.get('#player-list-display').should('contain', 'No name set');
+  });
+
+  it('joins an existing room', () => {
+    cy.get('#create-game-button').click();
+
+    cy.get('#join-game-button').click();
+
+    cy.get('#game-id-display')
+      .invoke('text')
+      .should('match', /Game ID: [a-z0-9]{4}/);
+
+    cy.get('input[name=gameId]')
+      .invoke('val')
+      .should('match', /[a-z0-9]{4}/);
+
+    cy.get('#player-list-display').should('contain', 'No name set');
   });
 
   it('leaves a room', () => {
@@ -33,5 +55,15 @@ describe('Main', () => {
     cy.get('#game-id-display').should('contain', 'Game ID: ');
 
     cy.get('input[name=gameId]').should('be.empty');
+  });
+
+  it('sets name', () => {
+    cy.get('input[name=name]').clear().type('wz');
+
+    cy.get('#set-name-button').click();
+
+    cy.visit('/');
+
+    cy.get('input[name=name]').should('have.value', 'wz');
   });
 });
