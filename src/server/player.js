92270b51c40937f0a3b50bc6ecd061ad870e086c
weizhongtan <me@wztan.com>
2020-10-23 20:54:39 +0100

    fix player syncing

--- src/server/player.js
+++ src/server/player.js
@@ -5,14 +5,33 @@ export class Player {
     this.name = 'No name set';
   }
 
+  setSocket(socket) {
+    this.socket = socket;
+    this.update();
+  }
+
+  setName(name) {
+    this.name = name;
+    this.update();
+  }
+
+  joinGame(gameId) {
+    this.gameId = gameId;
+    this.update();
+  }
+
   update() {
-    this.emit('set-name', this.name);
+    this.emit('sync', {
+      name: this.name,
+      gameId: this.gameId,
+    });
   }
 
   emit(tag, message) {
     this.socket.emit(tag, message);
     console.log(
-      `sending player ${this.id.substring(1, 6)} the message ${tag}: ${message}`
+      `sending player ${this.id.substring(1, 6)} the message ${tag}:`
     );
+    console.log(message);
   }
 }
